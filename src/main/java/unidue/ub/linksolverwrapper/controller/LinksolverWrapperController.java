a7f81d17e32a5676470b2b20c924424157cda603
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-04-26 11:42:00 +0200

    remove redis sessions as no sessions are needed, added additonal debug features, introduced case for fulltext links

--- src/main/java/unidue/ub/linksolverwrapper/controller/LinksolverWrapperController.java
+++ src/main/java/unidue/ub/linksolverwrapper/controller/LinksolverWrapperController.java
@@ -35,9 +35,7 @@ public class LinksolverWrapperController {
 
     private final static Logger log = LoggerFactory.getLogger(LinksolverWrapperController.class);
 
-    private final String modernDoiRegExp = "^doi:10.\\d{4,9}/[-._;()/:A-Za-z0-9]+$";
-
-    private final String oldWileyDoiRegExp = "^doi:10.1002/[\\S]+$";
+    private boolean isDoiUrl;
 
     // the address of the linksolver
     @Value("${libintel.linksolver.url}")
@@ -63,17 +61,21 @@ public class LinksolverWrapperController {
         log.info("call from " + remoteAddress);
         RedirectView redirectView = new RedirectView();
         String urlFromDoi = "";
-        String urlFromLinksolver = "";
+        String urlFromLinksolver;
         String doi = "";
         // first, check for DOI
         if (requestParams.containsKey("id")) {
+            log.debug("reading id parameters from request");
             List<String> ids = requestParams.get("id");
+            log.debug("found " + ids.size() + " id parameters");
             for (Object id : ids) {
                 String value = (String) id;
                 if (isDoi(value)) {
+                    log.debug(value + " idnetified as doi");
                     doi = value.replace("doi:", "");
                     urlFromDoi = RedirectLinkRetriever.getLinkForDoi(value);
                     log.info("retrieved link from DOI: " + urlFromDoi);
+                    this.isDoiUrl = !urlFromDoi.isEmpty();
                     redirectView.setUrl(urlFromDoi);
                 }
             }
@@ -81,32 +83,31 @@ public class LinksolverWrapperController {
         // retrieve availability information from linksolver
         try {
             String queryParameters = mapListToString(requestParams);
+            log.debug("getting response from linksolver");
             Document doc = Jsoup.connect(linksolverUrl + queryParameters).timeout(60 * 1000).get();
+            log.debug("found " + doc.select("a").size() + " links in linksolver response");
             for (Element link : doc.select("a")) {
                 String linkType = link.text();
                 log.info("linksolver returned option " + linkType);
+                if (linkType.startsWith("Volltexte "))
+                    linkType = linkType.substring(0, 9);
                 switch (linkType) {
                     case "Link zum Artikel": {
                         urlFromLinksolver = RedirectLinkRetriever.getLinkFromRedirect(linksolverUrl + link.attr("href"));
-                        log.info("retrieved link from linksolver: " + urlFromLinksolver);
+                        log.debug("retrieved link from linksolver: " + urlFromLinksolver);
                         log.info("full text available. Redirecting to resource.");
                         // check for shibboleth
-                        String url;
-                        if (!"".equals(urlFromDoi)) {
-                            log.info("trying to construct shibboleth link with doi link.");
-                            url = shibbolethBuilder.constructWayflessUrl(urlFromDoi, remoteAddress);
-                        } else {
-                            log.info("trying to construct shibboleth link with  linksolver link.");
-                            url = shibbolethBuilder.constructWayflessUrl(urlFromLinksolver, remoteAddress);
-                        }
+                        String url = getShibbolethUrl(urlFromDoi, urlFromLinksolver, remoteAddress);
                         // redirect to url
                         redirectView.setUrl(url);
                         return redirectView;
                     }
                     case "Fernleihe Zeitschriften": {
                         if (urlFromDoi.contains("sciencedirect") || urlFromDoi.contains("elsevier")) {
+                            log.debug("no fulltext available and elsevier journal. redirecting to order page.");
                             redirectView.setUrl("https://www.uni-due.de/ub/elsevierersatz.php?doi=" + doi);
                         } else {
+                            log.debug("no fulltext available. redirecting to interlibrary loan page");
                             requestParams.set("sid", "464_465:Zeitschriftenkatalog");
                             requestParams.set("pid", "<location>464_465<%2Flocation>");
                             requestParams.set("genre", "journal");
@@ -115,22 +116,36 @@ public class LinksolverWrapperController {
                         return redirectView;
                     }
                     case "Elektronischer und gedruckter Bestand der UB": {
+                        log.debug("printed or online access without doi. redirecting to journals online and print page.");
                         String issn = requestParams.getFirst("issn");
-                        if (issn.isEmpty())
-                            issn = requestParams.getFirst("eissn");
-                        if (!issn.isEmpty()) {
-                            Map<String, String> iopRequestParams = new HashMap<>();
-                            iopRequestParams.put("sid", "bib:ughe");
-                            iopRequestParams.put("pid", "bibid%3DUGHE");
-                            iopRequestParams.put("genre", "journal");
-                            iopRequestParams.put("issn", issn);
-                            // if "Link zum Artikel" is not present, redirect to the linksolver
-                            String url = "https://www.uni-due.de/ub/ghbsys/jop" + mapToString(iopRequestParams);
-                            redirectView.setUrl(url);
-                        } else
-                            redirectView.setUrl(linksolverUrl + queryParameters);
+                        if (issn != null)
+                            if (issn.isEmpty())
+                                issn = requestParams.getFirst("eissn");
+                        if (issn != null) {
+                            if (!issn.isEmpty()) {
+                                Map<String, String> iopRequestParams = new HashMap<>();
+                                iopRequestParams.put("sid", "bib:ughe");
+                                iopRequestParams.put("pid", "bibid%3DUGHE");
+                                iopRequestParams.put("genre", "journal");
+                                iopRequestParams.put("issn", issn);
+                                // if "Link zum Artikel" is not present, redirect to the linksolver
+                                String url = "https://www.uni-due.de/ub/ghbsys/jop" + mapToString(iopRequestParams);
+                                redirectView.setUrl(url);
+                            } else
+                                redirectView.setUrl(linksolverUrl + queryParameters);
+                        }
                         return redirectView;
                     }
+                    case "Volltexte":
+                        urlFromLinksolver = RedirectLinkRetriever.getLinkFromRedirect(linksolverUrl + link.attr("href"));
+                        log.info("retrieved link from linksolver: " + urlFromLinksolver);
+                        log.debug("full text available. Redirecting to resource.");
+                        // check for shibboleth
+                        String url = getShibbolethUrl(urlFromDoi, urlFromLinksolver, remoteAddress);
+
+                        // redirect to url
+                        redirectView.setUrl(url);
+                        return redirectView;
                 }
             }
         } catch (IOException e) {
@@ -142,6 +157,18 @@ public class LinksolverWrapperController {
         return redirectView;
     }
 
+    private String getShibbolethUrl(String urlFromDoi, String urlFromLinksolver, String remoteAddress) {
+        String url;
+        if (this.isDoiUrl) {
+            log.debug("trying to construct shibboleth link with doi link.");
+            url = shibbolethBuilder.constructWayflessUrl(urlFromDoi, remoteAddress);
+        } else {
+            log.debug("trying to construct shibboleth link with  linksolver link.");
+            url = shibbolethBuilder.constructWayflessUrl(urlFromLinksolver, remoteAddress);
+        }
+        return url;
+    }
+
     @GetMapping("/useShibboleth")
     public RedirectView useShibboleth(@RequestParam String target, HttpServletRequest request) {
         RedirectView redirectView = new RedirectView();
@@ -151,6 +178,8 @@ public class LinksolverWrapperController {
     }
 
     private boolean isDoi(String test) {
-    return (test.matches(modernDoiRegExp) || test.matches(oldWileyDoiRegExp));
+        String oldWileyDoiRegExp = "^doi:10.1002/[\\S]+$";
+        String modernDoiRegExp = "^doi:10.\\d{4,9}/[-._;()/:A-Za-z0-9]+$";
+        return (test.matches(modernDoiRegExp) || test.matches(oldWileyDoiRegExp));
     }
 }
